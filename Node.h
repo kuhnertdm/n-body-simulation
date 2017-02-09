c85ffa6b94a5f1389bd5a1e519be46dcc9e9f378
FFyuan <FFyuan@github.com>
2017-02-09 02:18:14 -0500

    Main structure finished. Working on Split

--- Node.h
+++ Node.h
@@ -1,18 +1,35 @@
+#pragma once
+
 #include "Object.h"
 #include "Vector3.h"
 #include <vector>
+#include "BoundingBox.h"
 
 class Node {
 public:
 	bool isLeaf;
 	Node *children[2][2][2];
-	Vector3 minBound;
-	Vector3 maxBound;
 	std::vector<Object*> objects;
+	BoundingBox bb;
 
 	void split(std::vector<Object*> objects) {
 		
 	}
 	
-	Node(std::vector<Object*> objects) {}
+	Node(std::vector<Object*> objects) {
+		int N = 1;
+		bb.minBound = objects[0]->position;
+		bb.maxBound = objects[0]->position;
+		for (int i = 0; i < objects.size(); i++) {
+			bb.expand(BoundingBox(objects[i]->position, objects[i]->position));
+		}
+		this->bb = bb;
+		if (objects.size <= N) {
+			this->isLeaf = true;
+			this->objects = objects;
+		}
+		else {
+			split(objects);
+		}
+	}
 };
---
+++ OrcTree.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Node.h"
+#include <vector>
+
+class OrcTree {
+public:
+	Node *root;
+
+	OrcTree() {}
+
+	OrcTree(std::vector<Object> &objects) {
+		this->root = new Node(objects);
+	}
+};
