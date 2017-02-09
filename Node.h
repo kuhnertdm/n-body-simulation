c9f84b60a1fb5aa1b98af21daa0fd77800847296
FFyuan <FFyuan@github.com>
2017-02-09 02:47:06 -0500

    First version of Construction. Debug and iterator(for traversal) in future

--- Node.h
+++ Node.h
@@ -13,7 +13,40 @@ public:
 	BoundingBox bb;
 
 	void split(std::vector<Object*> objects) {
-		
+		float splitx = (bb.maxBound.x + bb.minBound.x) / 2;
+		float splity = (bb.maxBound.y + bb.minBound.y) / 2;
+		float splitz = (bb.maxBound.z + bb.minBound.z) / 2;
+		std::vector<Object*> *values[2][2][2];
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 2; k++) {
+					values[i][j][k] = new std::vector<Object*>;
+				}
+			}
+		}
+		for (int i = 0; i < objects.size(); i++) {
+			int ix = 0;
+			int iy = 0;
+			int iz = 0;;
+			Object *ob = objects[i];
+			if (ob->position.x > splitx) {
+				ix = 1;
+			}
+			if (ob->position.y > splity) {
+				iy = 1;
+			}
+			if (ob->position.z > splitz) {
+				iz = 1;
+			}
+			values[ix][iy][iz]->push_back(ob);
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 2; k++) {
+					children[i][j][k] = new Node(*values[i][j][k]);
+				}
+			}
+		}
 	}
 	
 	Node(std::vector<Object*> objects) {
